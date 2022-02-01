8cc8420075907f0240e41a9608ab3603a05a2be7
gromanoalvarez <gromanoalvarez@alumno.unlam.edu.ar>
2022-02-01 01:08:19 -0300

    Eventos

---
+++ Angular/INTERACCIONES/DATA-BINDING.ts
@@ -0,0 +1,13 @@
+//! DATA BINDING >>>   [(ngModel)]="propiedadDeLaClase" 
+
+
+//TODO ej.      para modificar una propiedad (campo) en la vista y en el modelo o clase (.ts):
+
+//EN EL .HTML VISTA USO:
+// *          <input type="text" [(ngModel)]="campo" />
+
+// *     para mostrar lo que estoy modificando en la vista: 
+//          {{campo}}
+
+//EN EL .TS USO:
+//          public campo: string;
--- Angular/INTERACCIONES/eventos/EVENTOS.js
+++ Angular/INTERACCIONES/eventos/EVENTOS.js
@@ -1,4 +1,27 @@
+/**
+ *TODO                     Evento click - (click)="hacerClick()"
+ * 
+    En el .html uso en la etiqueta button
+        <button  (click)="hacerClick()" > CLICKEA AQUI </button>
+    En el .ts declaro el metodo 
+        hacerClick(){
+            alert('Diste click');
+        }
 
-
-//TODO           evento click
-//*                      (click)="metodo()"
+ *TODO                     Evento blur - (blur)="hasSalido()"
+ *
+ * * Captura "cuando salgo del campo"
+ *  usar [(ngModel)]="" para modificar una propiedad (campo) en la vista y en el modelo o clase (.ts)
+ *      <input type="text" [(ngModel)]="campo" (blur)="hasSalido()"/>
+ *      {{campo}}
+ * EN EL .TS 
+ *  hasSaldido(){alert('Saliste del campo');}
+ *
+ * 
+ *TODO                     Evento keyUp - (keyup.enter)="hasDadoAEnter()"
+ Cuando pulso una letra y la levanto
+        hasDadoAEnter(){
+            alert('Apretaste Enter');
+        }
+ * 
+ */
